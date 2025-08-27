ff0c90c315dc8a45f6d4972505b05472d9413415
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-08-27 10:58:27 +0000

    912

---
+++ app/components/Footer.js
@@ -0,0 +1,8 @@
+// components/Footer.js
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-8">
+      <p>&copy; {new Date().getFullYear()} Антон Меркуров. Все права защищены.</p>
+    </footer>
+  );
+}
