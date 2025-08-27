1c8e3009fabf7857bb516e4e4b4b5a6e629ffe2c
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-08-27 11:46:16 +0000

    112

--- app/Footer.js
+++ app/Footer.js
@@ -1,7 +1,7 @@
 // components/Footer.js
 import Link from 'next/link';
 import { supabase } from '@/lib/supabase-server';
-import { socialIcons } from './SocialIcons'; // Импортируем иконки
+import { socialIcons } from './components/Sociaicons'; // Импортируем иконки
 
 async function getSocialLinks() {
   const supabaseClient = supabase();
