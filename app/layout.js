2dd6b415be106d74d0685621da0f279d0ae3b21c
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-08-27 11:02:46 +0000

    914

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '@Header';
+import Footer from '@Footer';
 import { supabase } from '@/lib/supabase-server';
 
 export const metadata = {
