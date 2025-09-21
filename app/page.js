9fda5b21c767a18ea9d408a6ffc6c01885812d26
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-21 14:14:32 +0000

    gjgjh33433

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import { supabase } from '@/lib/supabase-server'; 
+// app/page.js
+import { createClient } from '../../lib/supabase-server'; // Замена алиаса на относительный путь
 import Link from 'next/link';
 
 async function getArticles() {
-  const supabaseClient = supabase(); 
-  
+  const supabaseClient = createClient(); // Используем createClient для создания клиента
+
   const { data, error } = await supabaseClient
     .from('articles')
     .select('id, title, created_at, content, slug')
@@ -24,7 +25,10 @@ export default async function HomePage() {
       <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {articles.length > 0 ? (
           articles.map((article) => (
-            <div key={article.id} className="bg-white rounded-lg shadow-sm hover:shadow-lg transition-shadow duration-300 p-6 border border-gray-100">
+            <div
+              key={article.id}
+              className="bg-white rounded-lg shadow-sm hover:shadow-lg transition-shadow duration-300 p-6 border border-gray-100"
+            >
               <Link href={`/articles/${article.slug}`}>
                 <h2 className="text-xl font-semibold text-gray-900 mb-2 hover:text-blue-600 transition-colors duration-200 cursor-pointer">
                   {article.title}
@@ -43,10 +47,24 @@ export default async function HomePage() {
                 {article.content}
               </p>
               
-              <Link href={`/articles/${article.slug}`} className="text-blue-500 font-medium hover:text-blue-400 transition-colors duration-300 inline-flex items-center">
+              <Link
+                href={`/articles/${article.slug}`}
+                className="text-blue-500 font-medium hover:text-blue-400 transition-colors duration-300 inline-flex items-center"
+              >
                 Читать далее
-                <svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-4 h-4 transform group-hover:translate-x-1 transition-transform" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ml-1 w-4 h-4 transform group-hover:translate-x-1 transition-transform"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M9 5l7 7-7 7"
+                  />
                 </svg>
               </Link>
             </div>
