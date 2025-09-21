05b8e9b6a77a0c5767e08a3497d30d431d21e920
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-21 16:53:08 +0000

    letsgo-1113

--- app/talks/page.tsx
+++ app/talks/page.tsx
@@ -1,6 +1,6 @@
 // app/talks/page.tsx
-import PasswordGuard from '@/components/talks/PasswordGuard';
-import LoungeInterface from '@/components/talks/LoungeInterface';
+import PasswordGuard from '@/components/PasswordGuard';
+import LoungeInterface from '@/components/LoungeInterface';
 
 // Отключаем статическую генерацию для этой страницы
 export const dynamic = 'force-dynamic';
