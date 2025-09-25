ccc8216f254c822fc5baa511038bb0dd7a9e7592
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-25 08:15:33 +0000

    auth

---
+++ components/AuthProvider.tsx
@@ -0,0 +1,14 @@
+// components/AuthProvider.tsx
+
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function AuthProvider({ children }: Props) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
