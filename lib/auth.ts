0e3d677690a8665888135904cdbf31b98ead1432
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-25 08:23:46 +0000

    auth-13

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+// lib/auth.ts
+
+import type { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+// Теперь мы экспортируем конфигурацию отсюда
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.sub as string;
+      }
+      return session;
+    },
+  },
+};
