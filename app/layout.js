a8b48bbb008654efdccc3005ab40a8ae8dbcb66f
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-26 20:08:39 +0000

    5555555

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 // app/layout.js
 
 import './main.css'; // <--- ИСПРАВЛЕННЫЙ ПУТЬ
+import './root-fix.module.css'; // <--- КРИТИЧЕСКИЙ ФИКС: ОБХОД WEBPACK
 import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
