6e9fc9e6a5404fb5541cb12e7c70d133621aaf5e
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-26 10:06:20 +0000

    userpics

--- next.config.js
+++ next.config.js
@@ -2,11 +2,19 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      // Правило для вашего хранилища Supabase
       {
         protocol: 'https',
         hostname: 'txvkqcitalfbjytmnawq.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
+      },
+      // Правило для аватаров Google
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
       },
     ],
   },
