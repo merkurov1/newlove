fba04b26b2c361db5c33dfa75bf6e22c17e3c58f
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-26 20:33:35 +0000

    claude2

--- next.config.js
+++ next.config.js
@@ -1,35 +1,25 @@
-/** @type {import('next').NextConfig} */
+/** @type {import(‘next’).NextConfig} */
 const nextConfig = {
-  // Убедитесь, что здесь нет экспериментальных флагов, таких как nextScriptWorkers.
-  reactStrictMode: true, 
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'txvkqcitalfbjytmnawq.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/a/**',
-      },
-    ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // ФИНАЛЬНЫЙ ФИКС: Перенаправляем устаревший импорт NextAuth на новый путь
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Это предотвращает ошибку 'next-auth/next is deprecated' в клиентской сборке
-      config.resolve.alias['next-auth/next'] = 'next-auth/react';
-    }
-    return config;
-  },
+reactStrictMode: true,
+images: {
+remotePatterns: [
+{
+protocol: ‘https’,
+hostname: ‘txvkqcitalfbjytmnawq.supabase.co’,
+port: ‘’,
+pathname: ‘/storage/v1/object/public/**’,
+},
+{
+protocol: ‘https’,
+hostname: ‘lh3.googleusercontent.com’,
+port: ‘’,
+pathname: ’/a/**’,
+},
+],
+},
+eslint: {
+ignoreDuringBuilds: true,
+},
 };
 
 module.exports = nextConfig;
-
