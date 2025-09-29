36d0513bfb17828b796c3faf239cf6d21351180a
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-29 09:22:52 +0000

    adminka-final-1

---
+++ app/admin/projects/new/page.js
@@ -0,0 +1,12 @@
+// app/admin/projects/new/page.js
+import ContentForm from '@/components/admin/ContentForm';
+import { createProject } from '../../actions';
+
+export default function NewProjectPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-900 mb-8">Новый проект</h1>
+      <ContentForm saveAction={createProject} type="проект" />
+    </div>
+  );
+}
