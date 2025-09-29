e85411cd71160c0c3359da97334d766211709197
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-29 06:57:01 +0000

    projects-gallery

--- components/MarkdownImage.js
+++ components/MarkdownImage.js
@@ -1,17 +1,19 @@
 // components/MarkdownImage.js
 import Image from 'next/image';
 
-// Этот компонент будет заменять стандартный тег <img>
 const MarkdownImage = (props) => {
   return (
-    <Image
-      src={props.src}
-      alt={props.alt}
-      width={1200} // Задаем большую ширину по умолчанию
-      height={675} // и высоту (соотношение 16:9)
-      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Адаптивные размеры
-      className="my-8 rounded-lg shadow-md transition-transform duration-300 hover:scale-[1.02]" // Стили для красоты
-    />
+    // Добавляем mb-8 (отступ снизу) и break-inside-avoid-column
+    <div className="mb-8 break-inside-avoid-column">
+      <Image
+        src={props.src}
+        alt={props.alt}
+        width={1200}
+        height={675}
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        className="rounded-lg shadow-md transition-transform duration-300 hover:scale-[1.02]"
+      />
+    </div>
   );
 };
 
