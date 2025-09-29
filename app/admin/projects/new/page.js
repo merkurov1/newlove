66bb1e46a06f4c0f1e1cfa60899f01d89f88b5d6
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-09-29 09:10:50 +0000

    adminka-fix

--- app/admin/projects/new/page.js
+++ app/admin/projects/new/page.js
@@ -6,7 +6,6 @@ export default function NewProjectPage() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900 mb-8">Новый проект</h1>
-      {/* Используем наш универсальный компонент, как и везде */}
       <ContentForm saveAction={createProject} type="проект" />
     </div>
   );
