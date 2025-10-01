2f29c07428d656dfd2f50ec132572d1219c358a2
merkurov1 <97518066+merkurov1@users.noreply.github.com>
2025-10-01 13:58:11 +0000

    refactoring

--- lib/middleware/auth.ts
+++ lib/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/lib/authOptions';
-import { Role } from '@/types/next-auth';
+import { Role } from '@/types/role';
 
 export async function requireAdmin(req: NextRequest) {
   const session = await getServerSession(authOptions);
